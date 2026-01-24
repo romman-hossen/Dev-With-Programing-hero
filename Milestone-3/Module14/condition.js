770f1e5660b3102fffc3841217f2557850ee4766
DevByteR <mdromman417@gmail.com>
2026-01-24 19:13:03 +0600

    Exploring condintion in js

---
+++ Milestone-3/Module14/condition.js
@@ -0,0 +1,54 @@
+// const monyBagHave = 2000;
+// if (monyBagHave >2000){
+//     console.log("I am boroloks");
+// }
+// else{
+//     console.log("Fokir sala !!")
+// }
+const salary = 30000;
+// const groom  ="kala";
+// const groom  ="shamla";
+
+const haveBsc = false;
+// if(salary>25000){
+//     console.log("Biya laga taratari");    
+// }
+// else{
+//     console.log("Vag sala !!")
+// }
+
+/** code to be executed when every condition is true/right */
+// if(salary>25000 && haveBsc==false){
+//     console.log("Biya laga taratari");    
+// }
+// else{
+//     console.log("Vag sala !!")
+// }
+
+/*** code to be exececuted when just 1 condition is right*****/
+// if(salary>25000 || haveBsc==false){
+//     console.log("Biya laga taratari");    
+// }
+// else{
+//     console.log("Vag sala !!")
+// }
+
+/** if condition have multiple time then 
+ * when you use compearesion and operation then make sure all code is okay !!
+ * if  you compeare or operation on multiple condition then only 1 condition is okay then code will be executed
+ */
+if(salary >=30000 && groom != "kala" && haveBsc){
+    console.log("Biya confirm tarikh thik kor!!")
+} 
+else{
+    console.log("Dure jaie muri kha !!");
+}
+
+/*** complex condition*** */
+
+
+
+
+
+
+
