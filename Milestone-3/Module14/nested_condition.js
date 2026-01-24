917e29bbeea1a2c26a40ffdae1c960d4876bd193
DevByteR <mdromman417@gmail.com>
2026-01-25 00:46:17 +0600

    Exploring nasted if else

---
+++ Milestone-3/Module14/nested_condition.js
@@ -0,0 +1,28 @@
+const taka = -1000;
+if(taka > 500){
+    console.log('Boroloks')
+}
+else{
+    if(taka > 300){
+        console.log('Moddhobitto')
+    }
+    else{
+        if(taka > 100){
+            console.log('Gorib');
+        }
+        else{
+            if(taka >50){
+                console.log('Fokir')
+            }
+            else{
+                if(taka > 0)
+                {
+                    console.log('Miskin')
+                }
+                else{
+                    console.log('Sorbohara')
+                }
+            }
+        }
+    }
+}
