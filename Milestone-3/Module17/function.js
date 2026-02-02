11d6fda18a1a543bc30f3c5f101645787672c869
DevByteR <mdromman417@gmail.com>
2026-02-02 16:23:55 +0600

    exploring function on js

---
+++ Milestone-3/Module17/function.js
@@ -0,0 +1,49 @@
+function myFunctiomn(){
+    console.log('Ghum theke otho')
+    console.log('tarpor brush koro ')
+    console.log('fena felo')
+    console.log('moblie e hat diba nah')
+}
+// myFunctiomn();
+
+function sumNumber(a,b)
+{
+    const sum  = a+b;
+    console.log(sum)
+    return sum;
+}
+sumNumber(4,5);
+console.log('__________________')
+console.log('__________________')
+console.log('__________________')
+sumNumber(40,56);
+function mulNumber(a,b,c,d,e)
+{
+    const mul  = a*b*c*d*e;
+    console.log(a,b,c,d,e)
+    console.log(mul)
+    return mul;
+}
+console.log('-------------!!')
+mulNumber(4,5,6,3,0)
+
+function squre(num)
+{
+    console.log(num)
+    num = num ** 2;
+    console.log(num)
+}
+// squre(11)
+function diff(num1,num2)
+{
+    console.log(num1,num2)
+    let num = num1 - num2;
+    console.log(`the difference betweem ${num1} - ${num2} is ${num}`)
+}
+let no1 = 450;
+let no2 = 89;
+diff(no1,no2)
+
+
+
+
---
+++ Milestone-3/Module17/return.js
@@ -0,0 +1,8 @@
+function double10X(num){
+    const double = num * 10;
+    return double;
+}
+// console.log(double10X(45))
+double10X(30);
+const tenX = double10X(60);
+console.log(tenX)
