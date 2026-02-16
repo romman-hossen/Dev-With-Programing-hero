e772cbafe86e7a99165e16bb1bf95d45caadc1cd
romman-hossen <mdromman417@gmail.com>
2026-02-17 00:30:27 +0600

    exploring event handling

---
+++ Milestone4/Module23/js/handel.js
@@ -0,0 +1,7 @@
+  document.getElementById('change').addEventListener('click',function(){
+        // console.log('hello')
+        const changeable = document.getElementById('value');
+        changeable.innerText = 'I am changing';
+        
+      })
+      
