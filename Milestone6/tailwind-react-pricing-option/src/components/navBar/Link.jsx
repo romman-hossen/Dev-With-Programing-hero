b78721c050cda61332f29e82de978b515d77be5a
DevByteR <mdromman417@gmail.com>
2026-03-27 17:48:00 +0600

    Creating navbar like daisy ui

--- Milestone6/tailwind-react-pricing-option/src/components/navBar/Link.jsx
+++ Milestone6/tailwind-react-pricing-option/src/components/navBar/Link.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Link = ({ route }) => {
   return (
-    <li className="mr-8">
+    <li className="md:mr-8 hover:bg-amber-200 text-black ">
       <a href={route.path}>{route.name}</a>
     </li>
   );
--- Milestone6/tailwind-react-pricing-option/src/components/navBar/NavBar.jsx
+++ Milestone6/tailwind-react-pricing-option/src/components/navBar/NavBar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "./Link";
+import { Menu, X } from "lucide-react";
 
 const NavBar = () => {
 
+  const [open,setOpen] = useState(false)
   const navLinks = [
     {
       id: 1,
@@ -30,12 +32,21 @@ const NavBar = () => {
       path: "/contact",
     },
   ];
+  const links = navLinks.map(route => <Link route ={route}></Link>)
 
   return (
-    <div>
+    <nav className="flex justify-between mx-6 my-4">
+      <span className="flex gap-2" onClick={() => setOpen(!open)}>
+        {open ? <X className="md:hidden"></X>: <Menu className="md:hidden"></Menu>}
+        <ul className={`md:hidden absolute duration-1000 px-6 space-y-2 py-1 bg-blue-900 ${open?'top-10' : '-top-60'}`}>
+          {links}
+        </ul>
+        <h3>My Navbar</h3>
+      </span>
+   
         {/* {navLinks.map(item => <li key={item.id}><a href={item.path}>{item.name}</a></li>)} */}
-        <ul className="flex">
-        {navLinks.map(route => <Link route ={route}></Link>)}
+        <ul className="md:flex hidden">
+         {links}
         </ul>    
       {/*static navbar*/}
       {/* <ol className='flex gap-5 list-none justify-center'>
@@ -43,7 +54,8 @@ const NavBar = () => {
          <li><a href="#">About</a></li>
          <li><a href="#">Configure</a></li>  
          </ol> */}
-    </div>
+         <button>Sign Up</button>
+    </nav>
   );
 };
 
