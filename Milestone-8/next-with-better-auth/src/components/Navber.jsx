758317dbb45a3c55b7ffad77a795cbe129b55a0e
DevByteR <mdromman417@gmail.com>
2026-05-02 21:42:52 +0600

    Better auth  sign up mongoDb added

---
+++ Milestone-8/next-with-better-auth/src/components/Navber.jsx
@@ -0,0 +1,25 @@
+import { Button } from '@heroui/react';
+import Link from 'next/link';
+import React from 'react';
+
+const Navber = () => {
+    return (
+        <div>
+            <nav className='flex justify-between container mx-auto p-4 items-center'>
+                <div><span className='text-2xl font-bold'>LOGO</span></div>
+                <div>
+                    <ul>
+                        <Link href="/" className='border-b'>Home </Link>
+
+                    </ul>
+                </div>
+                <Link href="/auth/signup">
+                     <Button variant="outline" className='text-white'>SignUP</Button>
+                </Link>
+
+            </nav>  
+        </div>
+    );
+};
+
+export default Navber;
