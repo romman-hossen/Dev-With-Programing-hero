3c10fda936cdb6e53b46bb663d7937b81fb879cf
DevByteR <mdromman417@gmail.com>
2026-05-13 23:33:36 +0600

    news section added

---
+++ Milestone-9/nextjs-user/src/app/useres/news/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NewsPage = () => {
+    return (
+        <div>
+            <h2>This is news page to explore all news today and like as a devloper what is wrong to slice to understand</h2>
+            
+              
+        </div>
+    );
+};
+
+export default NewsPage;
