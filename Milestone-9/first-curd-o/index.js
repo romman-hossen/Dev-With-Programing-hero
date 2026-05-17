b4d8297d7a675ea1173c2a228d1083bff52702fb
DevByteR <mdromman417@gmail.com>
2026-05-18 02:44:42 +0600

    Crud delete exploring

--- Milestone-9/first-curd-o/index.js
+++ Milestone-9/first-curd-o/index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app  = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5001;
 
 app.use(cors(),express.json());
 
-const uri = `mongodb+srv://SimpleCrudUser:VaI7wiDZLAV1OJoj@cluster0.rsp6gjh.mongodb.net/?appName=Cluster0`
+// const uri = process.env.MONGODB_URI;
 // MONGODB_URI = mongodb+srv://<db_username>:<db_password>@cluster0.rsp6gjh.mongodb.net/?appName=Cluster0
 // mongo-URI = SimpleCrudUser
 // mongo-pass = VaI7wiDZLAV1OJoj
+const uri = `mongodb+srv://SimpleCrudUser:VaI7wiDZLAV1OJoj@cluster0.rsp6gjh.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -29,13 +30,30 @@ const run =async () =>{
       const result = await cursor.toArray();
       res.send(result);
     })
+    app.get('/users/:id',async(req,res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id)
+      }
+      const user = await userCollection.findOne(query)
+      console.log('req',id)
+      res.send(user)     
+    })
+    app.delete('/users/:id',async(req,res) => {
+       const id = req.params.id;
+        const query = {
+        _id: new ObjectId(id)
+      }
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    // await client.close();  
   }
 
 }
